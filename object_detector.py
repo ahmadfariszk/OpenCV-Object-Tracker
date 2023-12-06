6c338b989affa7f650edf79c10a0b9dae92156fc
Ahmad Faris <149571714+ahmadfariszk@users.noreply.github.com>
2023-12-07 00:44:12 +0800

    Complted the object detection part
    - processed image into blobs

--- object_detector.py
+++ object_detector.py
@@ -9,10 +9,16 @@ classes = []
 with open("source_code\dnn_model\classes.txt", "r") as f:
     classes = [line.strip() for line in f.readlines()]   #put file contains into an array, by line
     layer_names = net.getLayerNames()
-    otputlayers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
+    output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers()]
 
 #Load image
 img = cv2.imread("cycling001-1024x683.jpg")
+
+#blob blob (this is the part that detects the object)
+blob = cv2.dnn.blobFromImage(img, 0.00392, (416,416), (0,0,0), True, crop=False)
+
+net.setInput(blob)
+outs = net.forward(output_layers)
 
 cv2.imshow("Image", img)
 cv2.waitKey(0)
