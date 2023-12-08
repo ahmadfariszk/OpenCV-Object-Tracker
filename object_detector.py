927976bb64bf7b0d92ad229c3b5f284b78245745
Ahmad Faris <149571714+ahmadfariszk@users.noreply.github.com>
2023-12-08 15:23:49 +0800

    Added rectangles around the object detected

--- object_detector.py
+++ object_detector.py
@@ -13,12 +13,32 @@ with open("source_code\dnn_model\classes.txt", "r") as f:
 
 #Load image
 img = cv2.imread("cycling001-1024x683.jpg")
+height, width, channels = img.shape
 
 #blob blob (this is the part that detects the object)
 blob = cv2.dnn.blobFromImage(img, 0.00392, (416,416), (0,0,0), True, crop=False)
-
 net.setInput(blob)
 outs = net.forward(output_layers)
+
+#Framing the detected objects with identifiers
+for out in outs:               #iterate through the outputs of all layers (outs)
+    for detection in out:      #iterate through all the detected objects within each layer
+        scores = detection[5:]
+        class_id = np.argmax(scores)   #to find what class is the detected object
+        confidence = scores[class_id]  #calculate confidence
+        if confidence > 0.5:           #threshold for when object is count as "detected"
+            center_x = int(detection[0] * width)
+            center_y = int(detection[1] * height)
+            w = int(detection[2] * width)
+            h = int(detection[2] * height)
+            
+            #checking what object are detected and where the centre opf the object is. uncomment to make it work
+            # cv2.circle(img,(center_x, center_y), 10, (0,255,0),2)
+
+            #object rectangle
+            x = int (center_x - w/2)
+            y = int (center_y - h /2)
+            cv2.rectangle(img,(x, y), (x+w,y+h), (0,255,0),2)
 
 cv2.imshow("Image", img)
 cv2.waitKey(0)
